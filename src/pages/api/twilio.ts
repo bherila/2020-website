84e8717d64e88df753e78d6b985678a7d1ebe886
Ben Herila <ben@herila.net>
2020-08-02 01:09:15 -0700

    Fax send a sms after receiving, upload to s3 bucket

--- src/pages/api/twilio.ts
+++ src/pages/api/twilio.ts
@@ -1,11 +1,60 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import twilio from 'twilio'
+import AWS from 'aws-sdk'
+import { PutObjectOutput } from 'aws-sdk/clients/s3'
 require('dotenv').config()
 
 const TwilioHandler = async (req: NextApiRequest, res: NextApiResponse) => {
   try {
     if (req.method === 'POST') {
       res.statusCode = 200
+      if (req.query.action === 'sent') {
+        // Let's manually build some TwiML. We can choose to receive the
+        // fax with <Receive>, or reject with <Reject>.
+        const twiml = `<Response><Receive action="/api/twilio?action=recv"/></Response>`
+
+        // Send Fax twiml response
+        res.setHeader('Content-type', 'text/xml')
+        return res.send(twiml)
+      } else if (req.query.action === 'recv') {
+        // notify us via sms!!
+        const [twilioSid, twilioSecret] = process.env.TWILIO.toString().split(
+          '.'
+        )
+        const api = twilio(twilioSid, twilioSecret)
+        const sms = api.messages.create({
+          body: req.body.FaxSid,
+          from: '+14158622534',
+          to: '+1' + 2 * 4544415689,
+        })
+
+        // download fax from Twilio
+        const faxFileArray = await fetch(req.body.MediaUrl)
+
+        // upload to s3
+        const [awsKeyId, awsSecret] = process.env.TWILIO.toString().split('.')
+        const s3 = new AWS.S3({
+          endpoint: 'sfo2.digitaloceanspaces.com',
+          accessKeyId: awsKeyId,
+          secretAccessKey: awsSecret,
+        })
+        const params = {
+          Body: await faxFileArray.arrayBuffer(),
+          Bucket: 'bwh',
+          Key: `fax${req.body.FaxSid}.pdf`,
+        }
+        const uploadResult = new Promise<PutObjectOutput>((resolve, reject) => {
+          s3.putObject(params, (err, data) => {
+            if (err) reject(err)
+            else resolve(data)
+          })
+        })
+
+        return res.json({
+          sid: (await sms).sid,
+          result: (await uploadResult).ETag,
+        })
+      }
       return res.json({})
     } else if (req.method === 'GET') {
       res.statusCode = 200
