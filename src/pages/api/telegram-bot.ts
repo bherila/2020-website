0b4f143ac5257f53378ea3cc9da9b1390c5d8702
Ben Herila <ben@herila.net>
2021-03-28 00:28:53 -0700

    Phabricator chatbot

---
+++ src/pages/api/telegram-bot.ts
@@ -0,0 +1,75 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { NextApiRequest, NextApiResponse } from 'next'
+import TelegramBot from 'node-telegram-bot-api'
+
+import * as phabricatorCtrl from '../../phabricator'
+
+const config = {
+  token: process.env.TOKEN,
+}
+
+const staticBot = (async () => {
+  const bot = new TelegramBot(config.token)
+  await bot.setWebHook('')
+
+  // Listener (handler) for telegram's /fileatask event
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the message text
+  bot.onText(/\/fileatask/, (msg, match) => {
+    const messageString = match.input
+    const chatId = msg.chat.id
+    const taskName = messageString.substr(messageString.indexOf(' ') + 1) //match.input.split(' ')[1];
+    const senderUsername = msg.from.username //default assignee is sender
+    if (!taskName) {
+      bot.sendMessage(chatId, 'Please provide task name')
+      return
+    }
+
+    const username = taskName.split('@')[1]
+    const reqParams = {
+      title: taskName,
+      assigneeUsername: username || senderUsername, // assign to username, if given.
+      senderUsername: senderUsername,
+      taskName: taskName?.split('@')[0],
+    }
+
+    //call create task api
+    phabricatorCtrl
+      .creatTask(reqParams)
+      .then(({ error_code, message }) => {
+        if (error_code) {
+          bot.sendMessage(chatId, 'Error occurred: ' + error_code)
+        } else {
+          bot.sendMessage(chatId, message, { parse_mode: 'HTML' })
+        }
+      })
+      .catch((err) => {
+        bot.sendMessage(chatId, 'Error' + err?.toString())
+      })
+  })
+
+  // Listener (handler) for telegram's /mytasks event
+  bot.onText(/\/mytasks/, (msg, match) => {
+    const chatId = msg.chat.id
+    const senderUsername = msg.from.username //default assignee is sender
+    //call list task api
+    phabricatorCtrl
+      .myTasks({ username: senderUsername })
+      .then(({ error_code, message }) => {
+        if (error_code) {
+          bot.sendMessage(chatId, 'Error occurred: ' + error_code)
+        }
+        bot.sendMessage(chatId, message, { parse_mode: 'HTML' })
+      })
+  })
+
+  return bot
+})()
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const aliveBot = await staticBot
+  aliveBot.processUpdate(req.body)
+  res.status(200)
+}
+
+export default handler
---
+++ src/phabricator.ts
@@ -0,0 +1,100 @@
+import got from 'got'
+const phabricatorURI = process.env.PHABRICATOR_URI || 'https://phab.bherila.net/api'
+const phabricatorToken = process.env.PHABRICATOR_API_TOKEN
+const userMap = [
+  {
+    telegramUser: 'bengwho',
+    phabricatorUser: 'bwh',
+    phabricatorPHID: 'PHID-USER-uxe5wuymmopwrrvaroud',
+  },
+  {
+    telegramUser: 'auerbachb',
+    phabricatorUser: 'bretton',
+    phabricatorPHID: 'PHID-USER-ctgxmxk5ycdossfbktfy',
+  },
+  {
+    telegramUser: 'lavkesh1608',
+    phabricatorUser: 'lavkesh',
+    phabricatorPHID: 'PHID-USER-qxytksntdxaqzfdtymat',
+  },
+]
+
+interface CreateTaskResult {
+  success: boolean
+  message?: string
+  error_code?: string
+}
+
+export async function creatTask(taskData): Promise<CreateTaskResult> {
+  const url = `${phabricatorURI}/maniphest.createtask`
+  const user = userMap.find((item) => item.telegramUser == taskData.username)
+  const body = {
+    'api.token': phabricatorToken,
+    title: taskData.title,
+    ownerPHID: user?.phabricatorPHID,
+  }
+  try {
+    const success = got.post(url, { form: body })
+    const response = JSON.parse(await success.json())
+    if (response.result) {
+      return {
+        success: false,
+        message: 'Task has been successfully created!',
+      }
+    } else {
+      console.error('success.body.error_code', response.error_code)
+      return { success: false, error_code: response.error_code }
+    }
+  } catch (error) {
+    console.error('catch block error in create task', error)
+  }
+  //{"result":null,"error_code":"ERR-INVALID-AUTH","error_info":"API token \"AAHYaH9zv9HT5vjBbwN-G1f7itdOUqmWsjg\" has the wrong length. API tokens should be 32 characters long."}
+  //{"result":{"id":"11","phid":"PHID-TASK-xss3u7w7dx6ilqzknqkl","authorPHID":"PHID-USER-qxytksntdxaqzfdtymat","ownerPHID":null,"ccPHIDs":["PHID-USER-qxytksntdxaqzfdtymat"],"status":"open","statusName":"Open","isClosed":false,"priority":"Needs Triage","priorityColor":"violet","title":"test task by lavkesh","description":"","projectPHIDs":[],"uri":"https:\/\/phab.bherila.net\/T11","auxiliary":[],"objectName":"T11","dateCreated":"1616504923","dateModified":"1616504923","dependsOnTaskPHIDs":[]},"error_code":null,"error_info":null}
+}
+
+interface MyTasksResult {
+  success: boolean
+  message?: string
+  error_code?: string
+}
+
+export async function myTasks(taskData): Promise<MyTasksResult> {
+  const url = `${phabricatorURI}/maniphest.search`
+  const body = {
+    'api.token': phabricatorToken,
+    'constraints[statuses][0]': 'open',
+  }
+  const user = userMap.find((item) => item.telegramUser === taskData.username)
+  let completeMessage = 'No task found.'
+  if (!user) {
+    return {
+      success: false,
+      message: completeMessage,
+    }
+  }
+
+  body['constraints[assigned][0]'] = user.phabricatorPHID
+  const success = await got.post(url, { form: body })
+  const response = JSON.parse(success.body)
+  if (response.result) {
+    const taskList = (response.result.data as any[])
+      .map(
+        (item) =>
+          `<a href="https://phab.bherila.net/T${item.id}">${item.fields.name}</a>`,
+      )
+      .join('\n')
+    if (taskList) {
+      completeMessage = 'Here are your open tasks. \n' + taskList
+      return {
+        success: true,
+        message: completeMessage,
+      }
+    } else {
+      return {
+        success: true,
+        message: 'You have no open tasks',
+      }
+    }
+  }
+  return { success: false, error_code: response.error_code }
+}
