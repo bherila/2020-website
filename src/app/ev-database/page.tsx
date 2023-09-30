60fd1ac353b2ebcea848dcf5c7af6f23ef8cc9d3
Ben Herila <ben@herila.net>
2023-09-30 15:52:44 -0700

    Add server-only for ev-database

--- src/app/ev-database/page.tsx
+++ src/app/ev-database/page.tsx
@@ -1,8 +1,11 @@
+import 'server-only'
 import mysql from '@/lib/db'
 import EvDatabaseClient from '@/app/ev-database/EvDatabaseClient'
 import React from 'react'
 import { Metadata } from 'next'
 import Typography from '@/components/typography'
+import Container from "@/components/container";
+import MainTitle from "@/components/main-title";
 
 export const metadata: Metadata = {
   title: 'The EV database',
@@ -16,12 +19,14 @@ export default async function EVDatabasePage() {
   const json = JSON.stringify(query)
   return (
     <>
-      <Typography variant="h1">The EV database</Typography>
-      <Typography variant="body1">
-        Inspired by a Reddit thread, I productionized the canonical EV database
-        spreadsheet into a real database, enabling some new use cases including
-        a JSON API, and more.
-      </Typography>
+      <Container>
+        <MainTitle>The EV database</MainTitle>
+        <Typography>
+          Inspired by a Reddit thread, I productionized the canonical EV database
+          spreadsheet into a real database, enabling some new use cases including
+          a JSON API, and more.
+        </Typography>
+      </Container>
       <EvDatabaseClient json={json} />
     </>
   )
