c2c71d2434fea136d736bb2041c98403f4e69aae
Ben Herila <ben@herila.net>
2024-11-10 17:45:25 -0800

    Remove ev-database

--- src/app/ev-database/ev-database-server.ts
+++
@@ -1,9 +0,0 @@
-import 'server-only'
-import mysql from '@/lib/db'
-import { EV } from '@/app/ev-database/ev-models'
-
-export async function loadEV(): Promise<EV[]> {
-  const query = await mysql.query('select * from ev_database')
-  await mysql.end()
-  return query as EV[]
-}
