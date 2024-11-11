42ada7aad7d715430af1c6ed7ec5a3d02a94e2c6
Ben Herila <ben@herila.net>
2024-11-11 00:28:25 -0800

    Use mysql2 https://github.com/jeremydaly/serverless-mysql/issues/96

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,6 @@
 import mysql from 'serverless-mysql'
 export default mysql({
+  library: require('mysql2'),
   config: {
     host: process.env.DBHOST,
     port: parseInt(process.env.DBPORT || '3306', 10),
