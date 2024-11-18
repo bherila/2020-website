518008e9221d2d87659b6edf1479984f9eaadae4
Ben Herila <ben+gh@herila.net>
2024-11-17 16:22:26 -0800

    Remove iron-session (no longer needed)

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,40 +1,48 @@
-import 'server-only'
-import type { IronSessionOptions } from 'iron-session'
-import { cookies } from 'next/headers'
-import { sealData, unsealData } from 'iron-session'
-import { sessionSchema, sessionType } from '@/lib/sessionSchema'
-import { cache } from 'react'
+import 'server-only';
+import { cookies } from 'next/headers';
+import { createCipheriv, createDecipheriv, randomBytes, scryptSync } from 'crypto';
+import { sessionSchema } from '@/lib/sessionSchema';
+import { cache } from 'react';
 
-const sessionOptions: IronSessionOptions = {
-  password: (process.env.VERCEL_ANALYTICS_ID as string) + 'cryptographically-strong pseudo random number generator',
-  cookieName: 'bwh-session',
-  // secure: true should be used in production (HTTPS) but can't be used in development (HTTP)
-  cookieOptions: {
-    secure: process.env.NODE_ENV === 'production',
-  },
+const algorithm = 'aes-256-cbc';
+const salt: string = 'FIcWDXjEtL03RmxSfRFvEHnj5cTwxZAbJluK9lrErXTttT9kDJkS8VM1hQjiJKB';
+const key = scryptSync(process.env.VERCEL_ANALYTICS_ID + salt, salt, 32);
+const cookieName = 'bwh-session';
+
+async function getSession_internal() {
+  const cookieStore = await cookies();
+  const encryptedSession = cookieStore.get(cookieName)?.value;
+  const session = encryptedSession ? decryptSession(encryptedSession) : null;
+  return session == null ? null : sessionSchema.parse(session);
+}
+export const getSession = cache(getSession_internal);
+
+function encryptSession(session: string) {
+  const iv = randomBytes(16);
+  const cipher = createCipheriv(algorithm, key, iv);
+  let encrypted = cipher.update(JSON.stringify(session), 'utf8', 'hex');
+  encrypted += cipher.final('hex');
+  return `${iv.toString('hex')}:${encrypted}`;
 }
 
-async function getSession_internal(): Promise<sessionType | null> {
-  const cookieStore = await cookies()
-  const encryptedSession = cookieStore.get(sessionOptions.cookieName)?.value
-  const session = encryptedSession
-    ? await unsealData(encryptedSession, {
-        password: sessionOptions.password,
-      })
-    : null
-  return session == null ? null : sessionSchema.parse(session)
-}
-export const getSession = cache(getSession_internal)
-
-export async function encryptSession(session: sessionType): Promise<string> {
-  return await sealData(session, { password: sessionOptions.password })
+function decryptSession(encrypted: string) {
+  try {
+    const [ivHex, encryptedData] = encrypted.split(':');
+    const decipher = createDecipheriv(algorithm, key, Buffer.from(ivHex, 'hex'));
+    let decrypted = decipher.update(encryptedData, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    return JSON.parse(decrypted);
+  } catch (err) {
+    console.error(err)
+    return null
+  }
 }
 
-export async function saveSession(session: sessionType | null) {
-  const cookieStore = await cookies()
+export async function saveSession(session: any) {
+  const cookieStore = await cookies();
   if (session == null) {
-    cookieStore.delete(sessionOptions.cookieName)
+    cookieStore.delete(cookieName);
   } else {
-    cookieStore.set(sessionOptions.cookieName, await encryptSession(session))
+    cookieStore.set(cookieName, encryptSession(session));
   }
 }
