f63f330acebfe3871fb5057f6c4b593bb62956ac
Ben Herila <ben+gh@herila.net>
2024-12-07 09:21:40 -0800

    add graphiql

---
+++ src/app/graphiql/page.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { GraphiQL } from 'graphiql'
+import 'graphiql/graphiql.css'
+
+export default function GraphiQLPage() {
+  const fetcher = async (graphQLParams: any) => {
+    const data = await fetch('/api/graphql', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(graphQLParams),
+      cache: 'no-store',
+      credentials: 'same-origin',
+    })
+    return data.json()
+  }
+
+  return (
+    <div style={{ height: '100vh' }}>
+      <GraphiQL fetcher={fetcher} />
+    </div>
+  )
+}
