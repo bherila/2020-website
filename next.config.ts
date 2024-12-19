3b38c57680937e0a105c7e979f5eb5ebbbbd91e1
Ben Herila <ben+gh@herila.net>
2024-12-19 15:59:12 -0800

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -41,6 +41,11 @@ export default (phase: any) => {
         destination: 'https://checkout.square.site/merchant/5170N93FK2SE4/checkout/3GQ3ACLAURLKM7FAUGNP7NEC',
         permanent: false,
       },
+      {
+        source: '/primerib25',
+        destination: 'https://bherilanet-my.sharepoint.com/:x:/g/personal/ben_herila_net/Ee3J5a3QS8hJq2w7976yr4YB5A-4oYAd3NqxBNuiwXL1Zg?e=JFVZwc',
+        permanent: false,
+      },
     ],
     // async headers() {
     //   return [
