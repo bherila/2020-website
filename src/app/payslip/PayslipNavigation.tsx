aa4ea66ad540274640a5a0e9a5c1c0b09fb5ae70
Ben Herila <ben+gh@herila.net>
2024-12-29 12:46:26 -0500

    Refactor payslip navbar

--- src/app/payslip/PayslipNavigation.tsx
+++ src/app/payslip/PayslipNavigation.tsx
@@ -7,24 +7,32 @@ export default function PayslipNavigation() {
   const pathname = usePathname()
 
   return (
-    <Nav variant="tabs" className="mb-3">
+    <Nav variant="tabs" className="mb-3 container-fluid mt-4 justify-content-center">
       <Nav.Item>
-        <Nav.Link as={Link} href="/payslip" active={pathname === '/payslip'}>
+        <Nav.Link
+          as={Link}
+          href="/payslip"
+          active={
+            pathname.startsWith('/payslip') &&
+            !pathname.startsWith('/payslip/import') &&
+            !pathname.startsWith('/payslip/config')
+          }
+        >
           List
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link as={Link} href="/payslip/import/pdf" active={pathname === '/payslip/import/pdf'}>
+        <Nav.Link as={Link} href="/payslip/import/pdf" active={pathname.startsWith('/payslip/import/pdf')}>
           Import PDF
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link as={Link} href="/payslip/import/json" active={pathname === '/payslip/import/json'}>
+        <Nav.Link as={Link} href="/payslip/import/json" active={pathname.startsWith('/payslip/import/json')}>
           Import JSON
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link as={Link} href="/payslip/config" active={pathname === '/payslip/config'}>
+        <Nav.Link as={Link} href="/payslip/config" active={pathname.startsWith('/payslip/config')}>
           Configuration
         </Nav.Link>
       </Nav.Item>
