f64052c1486fd1ae15acbdf7cb066c2d5443d0ed
Ben Herila <ben+gh@herila.net>
2024-12-30 12:56:31 -0500

    Remove some stuff not needed

--- src/app/payslip/page.tsx
+++ src/app/payslip/page.tsx
@@ -1,21 +1,20 @@
 import 'server-only'
-import { Atkinson_Hyperlegible } from 'next/font/google'
 import PayslipClient from './PayslipClient'
 import requireSession from '@/server_lib/requireSession'
 import Container from '@/components/container'
 import { Col, Row } from 'react-bootstrap'
 
-const atkinson = Atkinson_Hyperlegible({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-atkinson',
-})
+// const atkinson = Atkinson_Hyperlegible({
+//   weight: ['400', '700'],
+//   subsets: ['latin'],
+//   display: 'swap',
+//   variable: '--font-atkinson',
+// })
 
 export default async function PayslipPage() {
   await requireSession('/payslip')
   return (
-    <main className={atkinson.variable}>
+    <main>
       <Container>
         <div className="container my-2">
           <Row>
