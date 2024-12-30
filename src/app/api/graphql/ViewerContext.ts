27d89bc0603b1fd79c077560cab54d55a0434a23
Ben Herila <ben+gh@herila.net>
2024-12-29 23:07:56 -0500

    Remove graphql

--- src/app/api/graphql/ViewerContext.ts
+++
@@ -1,9 +0,0 @@
-import { BaseContext } from '@apollo/server'
-import { NextRequest } from 'next/server'
-
-type HandlerRequest = NextRequest | Request
-
-export default interface ViewerContext extends BaseContext {
-  uid: number
-  req: HandlerRequest
-}
